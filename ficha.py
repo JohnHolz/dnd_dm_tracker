805a9baf18d7306c71ac2a0babe8010ff5b9b4d4
João Holz <joaopaulo.paivaholz@gmail.com>
2021-10-01 13:17:28 -0300

    money class ok

---
+++ ficha.py
@@ -0,0 +1,42 @@
+{
+    # ! basic
+    'core': {
+        'name': 'choosen fire',
+        'class': 'novice',
+        'lvl':1,
+        'race': 'human',
+        'subraces': 'none',
+        'languages': {'common'},
+        'alignment': 'neutral',
+        'background':'soldier',
+    },
+
+    'appearance': {
+        'age': 20,
+        'size': 'medium',
+        'height': 0,
+        'weight': 0,
+        'skin': 'bege',
+        'eyes': 'brown',
+        'hair': 'black',
+    },
+
+    'flaws': 'dump',
+    'connections': {},
+    # ! keep track
+    'gold': 0,
+    'hp': 0,
+    'xp': 0,
+    # ! combat
+    'combat': {
+        'passive-perception': 11,
+        'CA': 10,
+        'initiative': 0,
+    },
+    # ! equipament
+    'equipament': {
+        'armor': {},
+        'weapon': {},
+        'itens': {},
+    },
+}

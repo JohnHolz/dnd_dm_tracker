d7ff14c1b6d31bbee769e796fcc36fe340511404
João Holz <joaopaulo.paivaholz@gmail.com>
2021-10-22 21:43:06 -0300

    removed 2 files

--- world/major/drow_dragon_princess.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('../../../src/')
-from npc.npc import Npc
-
-## Black drow dragons plot - NPC Enemies
-Takefuso = Npc('Takefuso')
-YoccastunToskan = Npc('Yoccastun Toskan')
